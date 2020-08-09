373678e76b9a180e0e357cc09af0faf95f8adc69
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-08-09 21:56:46 +0300

    first example added. i_am_rich application created and filled wiith simple text message

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// This is entry point for any Flutter application.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World!'),
+      ),
+    ),
+  );
+}
