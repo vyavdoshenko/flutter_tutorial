273665fb365b351a73df3087bf611224602b9c86
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-08-16 22:07:37 +0300

    Scaffolding a Material App

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWthuOqxSy0aIGfvUeFgcCifExVdsQ4HIacg&usqp=CAU'),
+          ),
+        ),
       ),
     ),
   );
