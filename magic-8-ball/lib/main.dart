d01665e97d76bc12d6621563d3009cd393a3a1d3
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-09-03 22:14:37 +0300

    magic ball ptoject. centered image added on body

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -11,14 +11,27 @@ class BallPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           backgroundColor: Colors.blue[900],
           title: Text('Ask Me Anything'),
         ),
-        body: Container(
-          color: Colors.blue,
-        ),
+        body: Ball(),
       ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
