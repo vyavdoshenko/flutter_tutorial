4ef2ea5957d8feb55839d22da70bdfcafdfd0274
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-09-03 16:54:34 +0300

    magic ball project added

---
+++ magic-8-ball/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Ask Me Anything'),
+        ),
+        body: Container(
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
