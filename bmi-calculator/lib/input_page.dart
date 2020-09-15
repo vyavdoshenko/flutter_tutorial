93341172ae376f384d6d6651bf6dd4be98a283c5
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-09-15 19:56:53 +0300

    bmi calculator project. slider widget and slider theme implemented

--- bmi-calculator/lib/input_page.dart
+++ bmi-calculator/lib/input_page.dart
@@ -92,17 +92,29 @@ class _InputPageState extends State<InputPage> {
                       ),
                     ],
                   ),
-                  Slider(
-                    activeColor: kActiveSliderColour,
-                    inactiveColor: kInactiveSliderColour,
-                    value: height.toDouble(),
-                    min: 120.0,
-                    max: 220.0,
-                    onChanged: (double newValue) {
-                      setState(() {
-                        height = newValue.round();
-                      });
-                    },
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: kInactiveSliderColour,
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape: RoundSliderThumbShape(
+                        enabledThumbRadius: 15.0,
+                      ),
+                      overlayShape: RoundSliderOverlayShape(
+                        overlayRadius: 30.0,
+                      ),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
