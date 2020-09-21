a20a25872c555b6949106a8a79b50349f3fb45cd
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-09-21 20:57:54 +0300

    clima project. get location on init method implemented

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -14,16 +14,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    getLocation();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
