d813eab95326ecbc9660ba8c86b9d56ff4ab080e
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2020-10-02 13:29:54 +0300

    todoey project. fix after refer on tutorial

--- todoey/lib/screens/add_task_screen.dart
+++ todoey/lib/screens/add_task_screen.dart
@@ -6,9 +6,7 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xFF757575),
       child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20.0,
-        ),
+        padding: EdgeInsets.all(20.0),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
@@ -16,44 +14,37 @@ class AddTaskScreen extends StatelessWidget {
             topRight: Radius.circular(20.0),
           ),
         ),
-        child: Container(
-          padding: EdgeInsets.only(
-            top: 40.0,
-            bottom: 20.0,
-            left: 20.0,
-            right: 20.0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Center(
-                  child: Text(
-                'Add Task',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            FlatButton(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                // required
+              },
+              child: Text(
+                'Add',
                 style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.lightBlueAccent,
-                ),
-              )),
-              Divider(
-                height: 40.0,
-                thickness: 5.0,
-                color: Colors.lightBlueAccent,
-              ),
-              FlatButton(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                color: Colors.lightBlueAccent,
-                onPressed: () {},
-                child: Text(
-                  'Add',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    //height: 20.0,
-                  ),
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  //height: 20.0,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
